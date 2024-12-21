3ff1dbbb2cb37714408fa55250c47d7869ece92a
MaryiaYesmanchyk <maryia.yesmanchyk@gmail.com>
2024-12-21 14:52:23 -0500

    Created header, nav and footer

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header.js'
+import Nav from "./components/Nav.js"
+import Main from "./components/Main.js"
+import Footer from "./components/Footer.js"
 
 function App() {
   return (
-    <div className="Homepage">
-    </div>
+    <>
+      <Header />
+      {/* <Nav/>
+      <Main/> */}
+      <Footer/>
+    </>
   );
 }
 
---
+++ capstone/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from '../assets/Logo.svg'
+
+function Footer (){
+    return(
+        <footer>
+            <img src={Logo} alt="Logo" />
+        </footer>
+    )
+}
+
+export default Footer;
